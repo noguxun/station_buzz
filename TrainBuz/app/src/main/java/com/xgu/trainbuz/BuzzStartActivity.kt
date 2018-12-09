20469592adc749a8895d8467863d02b4d22ca81a
guxun <noguxun@gmail.com>
2018-12-09 12:06:35 +0900

    first UI

---
+++ TrainBuz/app/src/main/java/com/xgu/trainbuz/BuzzStartActivity.kt
@@ -0,0 +1,12 @@
+package com.xgu.trainbuz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BuzzStartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buzz_start)
+    }
+}
